2a2fe54fedc53b2fbfd59d765b75db8b25ef01cf
adidokkk <sirtorigiovanni@gmail.com>
2026-03-31 00:55:53 -0500

    Fixed `write_data` in data_mem, added error flag signal, expanded comments

--- src/datapath.v
+++ src/datapath.v
@@ -1,4 +1,4 @@
-module datapath_top (
+module datapath (
     // External inputs
         input wire clka,
         input wire clkb,
@@ -16,7 +16,6 @@ module datapath_top (
         input wire mem_write,
         input wire mem_to_reg, // 0 = ALU/shifter result, 1 = memory data
         input wire shifter_en,
-        input wire counter_en,
         input wire [1:0] shifter_op,
         input wire out_port_en, // latch enable for 16 output pads
 
@@ -24,10 +23,11 @@ module datapath_top (
         output wire [3:0] opcode,
         output wire zero_flag, // Rs1 - Rs2 == 0
         output wire counter_done, // 1 = 16 cycles completed
-        output wire mmio_addr, // 1 = 0xFC00 (SW)
+        output wire mmio_addr, // 1 = 0xFC00 (SW), 0 = 0xFC02 (LW)
 
     // External outputs
-        output wire [15:0] out_port // output to MMIO (16 output pads)
+        output wire [15:0] out_port, // output to MMIO (16 output pads)
+        output wire error_flag_dp // 1 = invalid state or opcode
     );
 
     // Shifter opcodes
@@ -53,12 +53,14 @@ module datapath_top (
     wire [15:0] shifter_result;
     wire shifter_done;
     wire is_shift;
+    wire e_alu;
     
     assign zero_flag = (alu_result == 16'b0);
     assign branch_target = pc_out + imm;
     assign mmio_addr = (alu_result == 16'hFC00);
     assign counter_done = shifter_done;
     assign alu_in2 = alu_src ? imm : add2;
+    assign error_flag_dp = e_alu;
     assign is_shift = (opcode == OP_LSL) || (opcode == OP_LSR) ||
                       (opcode == OP_MAC) || (opcode == OP_CLZ);
     assign write_data = mem_to_reg ? mem_rdata : is_shift ? 
@@ -66,87 +68,96 @@ module datapath_top (
 
     // Program Counter (PC)
     pc pc_inst (
-            .clkb(clkb),
-            .rst(rst),
-            .pc_write(pc_write),
-            .pc_src(pc_src),
-            .branch_target(branch_target),
-            .pc_out(pc_out)
-        );
+        .clkb(clkb),
+        .rst(rst),
+        .pc_write(pc_write),
+        .pc_src(pc_src),
+        .branch_target(branch_target),
+
+        .pc_out(pc_out)
+    );
 
     // Instruction Memory
     ins_mem ins_mem_inst (
-            .addr(pc_out[3:0]),
-            .instr(instr)
-        );
+        .addr(pc_out[3:0]),
+
+        .instr(instr)
+    );
 
     // Instruction Register (IR)
     ir ir_inst (
-            .clkb(clkb),
-            .rst(rst),
-            .ir_load(ir_load),
-            .instr_in(instr),
-            .opcode(opcode),
-            .rd(rd),
-            .rs1(rs1),
-            .rs2(rs2),
-            .imm_raw(imm_raw)
-        );
+        .clkb(clkb),
+        .rst(rst),
+        .ir_load(ir_load),
+        .instr_in(instr),
+
+        .opcode(opcode),
+        .rd(rd),
+        .rs1(rs1),
+        .rs2(rs2),
+        .imm_raw(imm_raw)
+    );
 
     // Immediate Generator
     imm_gen imm_gen_inst (
-            .opcode(opcode),
-            .imm_raw(imm_raw),
-            .imm(imm)
-        );
+        .opcode(opcode),
+        .imm_raw(imm_raw),
+
+        .imm(imm)
+    );
 
     // Register File
     regfile regfile_inst (
-            .clkb(clkb),
-            .rst(rst),
-            .rs1(rs1),
-            .rs2(rs2),
-            .rd(rd),
-            .reg_write(reg_write),
-            .write_data(write_data),
-            .add1(add1),
-            .add2(add2),
-            .rd_data(rd_data)
-        );
+        .clkb(clkb),
+        .rst(rst),
+        .rs1(rs1),
+        .rs2(rs2),
+        .rd(rd),
+        .reg_write(reg_write),
+        .write_data(write_data),
+
+        .add1(add1),
+        .add2(add2),
+        .rd_data(rd_data)
+    );
 
     // ALU
     alu alu_inst (
-            .add1(add1),
-            .add2(alu_in2),
-            .alu_op(alu_op),
-            .result(alu_result)
-        );
+        .add1(add1),
+        .add2(alu_in2),
+        .alu_op(alu_op),
+
+        .result(alu_result),
+        .error_flag_alu(e_alu)
+    );
 
     // Shifter Unit
     shifter_unit shifter_inst (
-            .clkb(clkb),
-            .rst(rst),
-            .shifter_en(shifter_en),
-            .shifter_op(shifter_op),
-            .mul1(add1),
-            .mul2(add2),
-            .accum(rd_data),
-            .result(shifter_result),
-            .counter_done(shifter_done)
-        );
+        .clkb(clkb),
+        .rst(rst),
+        .shifter_en(shifter_en),
+        .shifter_op(shifter_op),
+        .mul1(add1),
+        .mul2(add2),
+        .accum(rd_data),
+
+        .result(shifter_result),
+        .counter_done(shifter_done)
+    );
 
     // Data Memory
     data_mem data_mem_inst (
-            .clkb(clkb),
-            .rst(rst),
-            .mem_read(mem_read),
-            .mem_write(mem_write),
-            .addr(alu_result),
-            .write_data(add2),
-            .in_port(in_port),
-            .out_port_en(out_port_en),
-            .read_data(mem_rdata),
-            .out_port(out_port)
+        .clkb(clkb),
+        .rst(rst),
+        .mem_read(mem_read),
+        .mem_write(mem_write),
+        .addr(alu_result),
+        .write_data(rd_data),
+        .in_port(in_port),
+        .out_port_en(out_port_en),
+        
+        .read_data(mem_rdata),
+        .out_port(out_port)
     );
 
 endmodule
