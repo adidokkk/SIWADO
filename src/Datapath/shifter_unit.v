d26734aff65413faee1b3f5898f129dbfbd74672
adidokkk <you@example.com>
2026-04-13 16:11:58 -0500

    Added 2's comp MAC feature, dual-clock interleaved structure, and short-circuit paths for one-cycle special cases

--- src/Datapath/shifter_unit.v
+++ src/Datapath/shifter_unit.v
@@ -1,9 +1,18 @@
-// Shifter Unit
-    // It handles MAC, CLZ, LSL, and LSR operations.
+// shift_reg Unit
+    // It handles signed MAC, CLZ, LSL, and LSR operations
+    // with dual-clock interleaved structure to maximize
+    // throughput.
+
+    // POTENTIAL OPTIMIZATIONS: 
+        //  1) CLZ combinational (priority encoder)
+        //  2) use comb. CLZ to skip lead. zeros in MAC
+    // However, making CLZ comb. would be too easy so 
+    // for ed. purposes we won't change it.
 
 module shifter_unit (
     // External inputs
-    input wire clk,
+    input wire clka,
+    input wire clkb,
     input wire rst,
 
     // Internal inputs from FSM
@@ -28,122 +37,210 @@ module shifter_unit (
     parameter OP_CLZ = 2'b11;
 
     reg active;
-    reg [1:0] cur_op;
-    reg [4:0] cycle_count;
+    reg [3:0] cycle_count;
 
     // MAC state
     reg [31:0] mac_accum;
-    reg [31:0] mac_mcand; // sign-extended mul1 (shifted left)
-    reg [15:0] mac_mult; // mul2 (shifted right)
+    reg [31:0] mac_mcand;
 
-    // CLZ state
-    reg [15:0] clz_val;
-    reg [4:0] clz_count;
+    reg [15:0] shift_reg;
 
-    always @(negedge clk)
+    reg [15:0] result_a, result_b;
+    reg counter_done_a, counter_done_b;
+    reg active_a, active_b;
+    reg [3:0] cycle_count_a, cycle_count_b;
+    reg [15:0] shift_reg_a, shift_reg_b;
+    reg [31:0] mac_accum_a, mac_accum_b;
+    reg [31:0] mac_mcand_a, mac_mcand_b;
+
+    always @(*)
+    begin
+        active = active_a || active_b;
+        counter_done = counter_done_a || counter_done_b;
+        result = result_a;
+
+        if (rst) begin
+            result = 0; 
+            cycle_count = 0;
+            shift_reg = 0;
+            mac_accum = 0;
+            mac_mcand = 0;
+        end else if (active) begin
+            if (counter_done)
+                result = clka ? result_b : clkb ? result_a : result;
+            else if (clkb) begin
+                cycle_count = cycle_count_a;
+                shift_reg = shift_reg_a;
+                mac_accum = mac_accum_a;
+                mac_mcand = mac_mcand_a;
+            end else if (clka) begin
+                cycle_count = cycle_count_b;
+                shift_reg = shift_reg_b;
+                mac_accum = mac_accum_b;
+                mac_mcand = mac_mcand_b;
+            end
+        end
+    end
+
+    always @(negedge clka)
     begin
         if (rst) begin
-            active <= 0;
-            counter_done <= 0;
-            result <= 16'b0;
-            cur_op <= 2'b0;
-            cycle_count <= 5'b0;
-            mac_accum <= 32'b0;
-            mac_mcand <= 32'b0;
-            mac_mult <= 16'b0;
-            clz_val <= 16'b0;
-            clz_count <= 5'b0;
+            result_a <= 16'b0;
+            counter_done_a <= 0;
+            active_a <= 0;
+            cycle_count_a <= 4'b0;
+            shift_reg_a <= 16'b0;
+            mac_accum_a <= 32'b0;
+            mac_mcand_a <= 32'b0;
         end else begin
-            counter_done <= 0;
+            counter_done_a <= 0;
 
             if (shifter_en && !active) begin // if not active, start multi-cycle operation
-                cur_op <= shifter_op;
-                cycle_count <= 5'b0;
+                cycle_count_a <= 4'b0001;
+                active_a <= ~counter_done; // ensure `shifter_en` goes low before lowering `active`
 
                 case (shifter_op)
-                    OP_LSL : begin
-                        result <= mul1;
-                        cycle_count <= {1'b0, mul2[3:0]}; 
-                        if (mul2[3:0] == 0)
-                            counter_done <= 1;
-                        else
-                            active <= 1;
-                    end
-
-                    OP_LSR : begin
-                        result <= mul1;
-                        cycle_count <= {1'b0, mul2[3:0]}; 
-                        if (mul2[3:0] == 0)
-                            counter_done <= 1;
-                        else
-                            active <= 1;
+                    OP_LSL, OP_LSR : begin
+                        if (mul2[3:0] == 0) begin
+                            result_a <= mul1;
+                            counter_done_a <= 1;
+                            active_a <= 0;
+                        end else begin
+                            shift_reg_a <= shifter_op == OP_LSL ? mul1 << 1 : mul1 >> 1;
+                        end
                     end
 
                     OP_MAC : begin
-                        mac_accum <= 32'b0;
-                        mac_mcand <= {{16{mul1[15]}}, mul1}; // sign-extended
-                        mac_mult <= mul2;
-                        counter_done <= counter_done;
-                        active <= counter_done ? 0 : 1;
+                        if (!mul1 || !mul2) begin
+                            result_a <= accum;
+                            counter_done_a <= 1;
+                            active_a <= 0;
+                        end else begin
+                            mac_accum_a <= mul2[0] ? {{16{mul1[15]}}, mul1} : 32'b0;
+                            mac_mcand_a <= {{16{mul1[15]}}, mul1} << 1;
+                            shift_reg_a <= mul2 >> 1;
+                        end
                     end
 
                     OP_CLZ : begin
-                        clz_val <= mul1;
-                        clz_count <= 5'b0;
-                        counter_done <= counter_done;
-                        active <= counter_done ? 0 : 1;
+                        if (mul1[15]) begin
+                            result_a <= 16'b0;
+                            counter_done_a <= 1;
+                            active_a <= 0;
+                        end else if (mul1 == 16'b0) begin
+                            result_a <= 16'h10;
+                            counter_done_a <= 1;
+                            active_a <= 0;
+                        end else begin
+                            shift_reg_a <= mul1 << 1;
+                        end
                     end
                 endcase
-            end else if (active) begin // if active, continue multi-cycle operation
-                case (cur_op)
-                    OP_LSL : begin
-                        result <= result << 1;
-                        if (cycle_count == 1) begin
-                            counter_done <= 1;
-                            active <= 0;
-                        end else begin
-                            cycle_count <= cycle_count - 1;
-                        end
-                    end
+            end else if (active) begin
+                active_a <= active_b;
 
-                    OP_LSR : begin
-                        result <= result >> 1;
-                        if (cycle_count == 1) begin
-                            counter_done <= 1;
-                            active <= 0;
+                case (shifter_op)
+                    OP_LSL, OP_LSR : begin                    
+                        if (cycle_count == mul2[3:0]) begin
+                            result_a <= shift_reg;
+                            counter_done_a <= 1;
+                            active_a <= 0;
                         end else begin
-                            cycle_count <= cycle_count - 1;
+                            cycle_count_a <= cycle_count + 1;
+                            shift_reg_a <= shifter_op == OP_LSL ? shift_reg << 1 : shift_reg >> 1;
                         end
                     end
 
                     OP_MAC : begin
-                        if (cycle_count < 5'b10000) begin // continue MAC till cycle_count = 16
-                            if (mac_mult[0])
-                                mac_accum <= mac_accum + mac_mcand;
-                            mac_mcand <= mac_mcand << 1; // advance position
-                            mac_mult <= mac_mult >> 1; // get next bit
-                            cycle_count <= cycle_count + 1;
+                        if (cycle_count < 4'b1111) begin // continue MAC till cycle_count = 15
+                            if (shift_reg[0])
+                                mac_accum_a <= mac_accum + mac_mcand;
+                            else
+                                mac_accum_a <= mac_accum;
+                            mac_mcand_a <= mac_mcand << 1; // advance position
+                            shift_reg_a <= shift_reg >> 1; // get next bit
+                            cycle_count_a <= cycle_count + 1;
                         end else begin
-                            result <= accum + mac_accum[15:0];
-                            counter_done <= 1;
-                            active <= 0;
+                            result_a <= accum + mac_accum[15:0] - (shift_reg[0] ? mac_mcand[15:0] : 16'b0); // handles signed 2's comp 
+                            counter_done_a <= 1;
+                            active_a <= 0;
                         end
                     end
 
                     OP_CLZ : begin
-                        if (clz_val[15] || clz_count == 5'b10000) begin // first '1' found or went through all bits
-                            result <= {11'b0, clz_count};
-                            counter_done <= 1;
-                            active <= 0;
+                        if (shift_reg[15] || cycle_count == 4'b1111) begin // first '1' found or went through all bits
+                            result_a <= {12'b0, cycle_count};
+                            counter_done_a <= 1;
+                            active_a <= 0;
                         end else begin
-                            clz_val <= clz_val << 1;
-                            clz_count <= clz_count + 1;
+                            shift_reg_a <= shift_reg << 1;
+                            cycle_count_a <= cycle_count + 1;
                         end
                     end
 
-                    default : active <= 0;
+                    default : active_a <= 0;
                 endcase
             end
+        end
+    end
+    
+    always @(negedge clkb)
+    begin
+        if (rst) begin
+            result_b <= 16'b0;
+            counter_done_b <= 0;
+            active_b <= 0;
+            cycle_count_b <= 4'b0;
+            shift_reg_b <= 16'b0;
+            mac_accum_b <= 32'b0;
+            mac_mcand_b <= 32'b0;
+        end
+        counter_done_b <= 0;
+
+        if (active) begin // if active, continue multi-cycle operation
+            active_b <= active_a;
+
+            case (shifter_op)
+                OP_LSL, OP_LSR : begin                    
+                    if (cycle_count == mul2[3:0]) begin
+                        result_b <= shift_reg;
+                        counter_done_b <= 1;
+                        active_b <= 0;
+                    end else begin
+                        cycle_count_b <= cycle_count + 1;
+                        shift_reg_b <= shifter_op == OP_LSL ? shift_reg << 1 : shift_reg >> 1;
+                    end
+                end
+
+                OP_MAC : begin
+                    if (cycle_count < 4'b1111) begin // continue MAC till cycle_count = 15
+                        if (shift_reg[0])
+                            mac_accum_b <= mac_accum + mac_mcand;
+                        else
+                            mac_accum_b <= mac_accum;
+                        mac_mcand_b <= mac_mcand << 1; // advance position
+                        shift_reg_b <= shift_reg >> 1; // get next bit
+                        cycle_count_b <= cycle_count + 1;
+                    end else begin
+                        result_b <= accum + mac_accum[15:0] - (shift_reg[0] ? mac_mcand[15:0] : 16'b0); // handles signed 2's comp 
+                        counter_done_b <= 1;
+                        active_b <= 0;
+                    end
+                end
+
+                OP_CLZ : begin
+                    if (shift_reg[15] || cycle_count == 4'b1111) begin // first '1' found or went through all bits
+                        result_b <= {12'b0, cycle_count};
+                        counter_done_b <= 1;
+                        active_b <= 0;
+                    end else begin
+                        shift_reg_b <= shift_reg << 1;
+                        cycle_count_b <= cycle_count + 1;
+                    end
+                end
+
+                default : active_b <= 0;
+            endcase
         end
     end
 
