9b5a6eb272569af0d78213b2f97c67fcdc202ea9
adidokkk <you@example.com>
2026-04-16 15:18:45 -0500

    Created tcl and bash files to extract outputs and get box size from Magic with no GUI

---
+++ magic/magic.tcl
@@ -0,0 +1,23 @@
+# Disable rescaling when reading GDS
+calma rescale no
+
+# Read the GDS file
+gds read ../implementation/top.gds
+load top
+
+# Flatten design
+flatten flattop
+load flattop
+save flattop
+
+# Check box size
+select top cell
+scalegrid 1 1
+box
+
+# Extract netlist and sim for IRSIM
+extract all
+ext2sim -t!
+
+# Exit magic
+quit -noprompt
